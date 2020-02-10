48d4a4181596525c568abd12994719ebc68aa79f
Dmitrii Slepnev <odmsl002@edu.xamk.fi>
2020-02-10 15:09:55 +0100

    Implemented features in AddActivity

--- app/src/main/java/com/slepnev/studentportal/AddActivity.kt
+++ app/src/main/java/com/slepnev/studentportal/AddActivity.kt
@@ -1,12 +1,39 @@
 package com.slepnev.studentportal
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_add.*
+
+const val EXTRA_PORTAL = "EXTRA_PORTAL"
+
 
 class AddActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add)
+        initViews()
     }
+
+    private fun initViews() {
+        btnAdd.setOnClickListener { onSaveClick() }
+    }
+
+    private fun onSaveClick() {
+        if (etTitle.text.toString().isNotBlank() && etUrl.text.toString().isNotBlank()) {
+            val portal = Portal(etTitle.text.toString(), etUrl.text.toString())
+            val resultIntent = Intent()
+            resultIntent.putExtra(EXTRA_PORTAL, portal)
+            setResult(Activity.RESULT_OK, resultIntent)
+            finish()
+        } else {
+            Toast.makeText(this,"You have to fill in both fields"
+                , Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
 }
