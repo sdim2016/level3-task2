b4d5b3c0e67c043985d985d3af7cddae08abbbc7
Dmitrii Slepnev <odmsl002@edu.xamk.fi>
2020-02-10 14:59:12 +0100

    Configured the data class to implement Parcelable

--- app/src/main/java/com/slepnev/studentportal/Portal.kt
+++ app/src/main/java/com/slepnev/studentportal/Portal.kt
@@ -1,6 +1,10 @@
 package com.slepnev.studentportal
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Portal (
     var title: String,
     var url: String
-)
+) : Parcelable
