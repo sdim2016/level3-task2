e6b6535683ae1985e041d7ac0d324a7b4965afdf
Dmitrii Slepnev <odmsl002@edu.xamk.fi>
2020-02-10 14:25:47 +0100

    Created the PortalAdapter

---
+++ app/src/main/java/com/slepnev/studentportal/PortalAdapter.kt
@@ -0,0 +1,29 @@
+package com.slepnev.studentportal
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_portal.view.*
+
+class PortalAdapter(private val portals: List<Portal>):RecyclerView.Adapter<PortalAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_portal, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return portals.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(portals[position])
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(portal:Portal) {
+            itemView.btnPortal.text = portal.title + "\n" + portal.url
+        }
+    }
+}
